71bec64d6ed2c330ef9cf0065d65eacd6a0e85fd
= <=>
2021-07-05 08:41:08 +0200

    Passing of return value works

--- conv.py
+++ conv.py
@@ -9,6 +9,10 @@
 # last change: 2021-06-29                                              #
 ########################################################################
 
+## call with
+## python conv.py -q .\data -p .\param -w .\work -i test.xml
+
+##TODO
 # # use Conv::Instructions;
 # # use Conv::Config;
 # # use Conv::Logfile;
@@ -18,6 +22,7 @@ from functools import partial
 from pathlib import Path
 from pprint import pprint
 from datetime import datetime
+# from shutil import copyfile
 ## application specfic modules
 import conv.FalconConvert 
 import conv.FalconIndex 
@@ -31,21 +36,32 @@ import conv.SysMerge
 ########################################################################
 
 def run_workflow(path, workflow, param, work):
-    file = str(path)  ### because path is object not string        
-    for ix, step in enumerate(workflow):
-        run_module( file, step, ix, param, work )
+    if os.path.isfile( path ):
+        file = str(path)  ### because path is object not string        
+        for ix, step in enumerate(workflow):
+            return_code = run_module( file, step, ix, param, work )
+            # pprint( return_code )
+            if return_code == 0:
+                continue
+            else:
+                print( 'Step not processed orderly.' ) #TODO error message
+                return Error
         
 def run_module( file, step, ix, param, work):
     module = step['@name']
     ## run respective module
-    exec( 'conv.' + module + '.run( file, step, ix, param, work)')         
+    return eval( 'conv.' + module + '.run( file, step, ix, param, work)' )   
 
 ########################################################################
 ###                           main                                   ###
 ########################################################################
 if __name__ == "__main__":
+    start = datetime.now().replace(microsecond=0)       
     ## process command line options
     parser=argparse.ArgumentParser()
+    # group = parser.add_mutually_exclusive_group(required=True)
+    # group.add_argument('--foo',action=.....)
+    # group.add_argument('--bar',action=.....)    
     parser.add_argument('-queue', help='')
     parser.add_argument('-q', help='')
     parser.add_argument('-work', help='' )
@@ -66,33 +82,22 @@ if __name__ == "__main__":
     parser.add_argument('-s', help='')
     parser.add_argument('-library', help='', default='$Conv::Config::MODULE_LIBRARY')
     parser.add_argument('-l', help='')
-    args=parser.parse_args()
-    print( args )
-    
-    ## add error handling
-    
-    ## instructions 
-    instr_in_str=args.instructions or args.i
-    # print(instr_in_str)       
-    xml_as_string = Path('test.xml').read_text() ## TODO
-    xml_as_dict = xmltodict.parse(xml_as_string, dict_constructor=dict)
-    wf = xml_as_dict['conversion']['workflow']['module']      
-    
-    ## directories
+    args=parser.parse_args()  
+    ## TODO: add error handling    
+    ## dirs
     work_dir_in_str=args.work or args.w
-    print(work_dir_in_str)    
     param_dir_in_str=args.param or args.p
-    print(param_dir_in_str)
-    data_dir_in_str=args.queue or args.q
-    print(data_dir_in_str)    
-    
-    pathlist = Path(data_dir_in_str).rglob('*.*')   
-    run_workflow_ = partial(run_workflow, workflow=wf, param=param_dir_in_str, work=work_dir_in_str)    
-    start = datetime.now().replace(microsecond=0)
-        
-    ## execute workflow on each path in pathlist
+    data_dir_in_str=args.queue or args.q    
+    instr_as_str=args.instructions or args.i      
+    ## instructions xml
+    xml_as_string = Path('test.xml').read_text() ## TODO xmltodict erkennt keine Kommentare
+    xml_as_dict = xmltodict.parse(xml_as_string, dict_constructor=dict)
+    wf = xml_as_dict['conversion']['workflow']['module']                
+    ## execute workflow on each path in pathlist    
+    pathlist = Path(data_dir_in_str).rglob('*.*')                
+    run_workflow_partial = partial(run_workflow, workflow=wf, param=param_dir_in_str, work=work_dir_in_str)       
     with multiprocessing.Pool() as pool:
-        results = pool.map(run_workflow_, pathlist) 
+        results = pool.map(run_workflow_partial, pathlist) 
         results = [x for x in results if x is not None ]
         results = [x for x in results if x != 0 ]
     # sys.exit()        
@@ -100,152 +105,3 @@ if __name__ == "__main__":
         # results
     print('This took in total: ')
     print(datetime.now().replace(microsecond=0)-start) 
-
-
-
-
-
-# import Conv:Instructions
-# import Conv::Config
-# import Conv::Logfile
-# # my $opt = Getopt::Lucid->getopt( \@option_spec );
-# # my $queue = add_slash($opt->get_queue)       || undef;
-# # my $work  = add_slash($opt->get_work)        || die "Missing mandatory command line argument -w";
-# # my $instr = $opt->get_instruction            || die "Missing mandatory command line argument -i";
-# # my $lib   = $opt->get_library                || $Conv::Config::MODULE_LIBRARY;
-# # my $pdir  = $opt->get_param                  || undef;
-# # my $dof   = $opt->get_die                    || undef;
-# # my $talk  = $opt->get_verbose                || undef;
-# # my $step  = $opt->get_step                   || undef;
-# # my $stwth = $opt->get_begin                  || 0;
-# # my $endat = $opt->get_end                    || undef;
-# # die "Missing mandatory command line argument -q" unless ($step || $queue);
-# # # create a file-util object
-# # my $fu = File::Util->new();
-# # # create objects for logfile and messages
-# # my $logfile    = Conv::Logfile->new(substr($instr,0,index($instr,'.xml'))."\t",[$FindBin::RealBin.$Conv::Config::LOGFILE,substr($instr,0,index($instr,'.xml')).".log"]);
-# # # predeclare variables
-# # my $upload_success = 0;
-# # my $upload_failed  = 0;
-# # my $cstatus        = 0;
-
-
-
-
-# # # check if the lock-file is set on the working directory
-# # if (-e $work.'isrunning.lock')
-# # {
-	# # $logfile->event("Attempt to start the converter process failed, another converter process is already running on $work.");	
-	# # print "Cannot start the conversion workflow, since another conversion process is already using this directory.\n" if ($talk);
-# # }
-# # else
-# # {
-	# # # the actual program
-	# # $logfile->event("Converter process started");
-
-	
-	# # # set the isrunning lockfile
-	# # $fu->write_file('file' => $work.'isrunning.lock', 'content' => "1");
-	
-	# # # create file list, set dummy filename for step mode
-	# # my @filelist = ('default');
-	
-	# # # in standard mode override file list with file names found in -q
-	# # unless ($step)
-	# # {
-		# # # get the records from $queue
-		# # @filelist    = $fu->list_dir($queue, qw/--files-only/);
-
-	# # }
-	# # my $no_of_files = $#filelist +1;
-	# # my $j           = 0;
-	# # print "$no_of_files files found in $queue\n" if ($talk);	
-
-		   
-	# # # process all files, one by one
-	# # foreach my $file (@filelist)
-	# # {
-
-		# # # file counter
-		# # $j++;
-		# # # copy file to working directory
-		# # copy ($queue.$file,$work.$file) unless ($step);
-		
-		# # # conversion
-		# # # get the instructions
-		# # my $inst         = Conv::Instructions->new($instr,$work,$queue,$file,$pdir);
-		# # my @instructions = $inst->get_instructions;
-
-		# # if (@instructions)
-		# # {
-			# # # set status to "success" (may be changed during processing)
-			# # $cstatus = 1;
-			
-			# # # set last module to be processed
-			# # my $run_until = $endat || $#instructions;
-			   # # $run_until = $#instructions if ($run_until > $#instructions);
-			   # # $run_until = $#instructions if ($run_until < $stwth);
-			
-			# # for (my $i = $stwth; $i <= $run_until; $i++)
-			# # {
-				# # my $instruction = $instructions[$i];
-				# # my $module_to_load = $lib."::".$instruction->{NAME};
-
-							
-				# # # try to call execute() from the loaded module
-				# # eval
-				# # {
-					# # load $module_to_load, 'execute';
-					# # execute($instruction,$work,$file);
-					# # print "executed $module_to_load on file $file [file $j out of $no_of_files, step $i of ".$#instructions."]\n" if ($talk);
-				# # }; 
-				# # # if failed, write error messages to logfile and quit current workflow
-				# # if ($@)
-				# # {
-					# # # nice error message for the logfile
-					# # my $errormsg = $@;
-		   			   # # $errormsg =~ s/\n/ /g;
-		   			
-		   			# # # log it, set status to "failed" and end the loop
-					# # $logfile->event("Processing of file $file failed in module $module_to_load: $errormsg");
-					# # print "Processing of file $file failed in module $module_to_load: $errormsg"  if ($talk);
-					# # $cstatus = 0;
-					# # $upload_failed++;
-					# # if ($dof) {die("Processing of file $file failed in module $module_to_load: $errormsg");} else {last;}
-				# # }
-			# # }
-			
-			# # # log success
-			# # if ($cstatus)
-			# # {
-				# # $logfile->event("Processing of file $file successfully completed");
-				# # $upload_success++;
-			# # }
-		# # }
-		# # else
-		# # {
-
-			# # # failed reading instruction file
-			# # $logfile->event("Processing of $file failed: Cannot read instruction file $instr");
-			# # $upload_failed++;
-		# # }
-		
-	# # }
-	
-	# # # printout messages
-	# # print "$upload_success files out of $no_of_files in $queue successfully processed using instructions from $instr.\n" if ($upload_success);
-	# # print "Processing failed for $upload_failed files out of $no_of_files in $queue using instructions from $instr.\n"   if ($upload_failed);
-	
-	# # # finish: remove lock-file
-	# # unlink $work.'isrunning.lock';
-	# # $logfile->event("Converter process stopped");
-# # }
-# # exit(1);
-
-# # sub add_slash
-# # {
-	# # # add a slash to the end of the path if there isn't one already
-	# # my $pathname = shift;
-	# # $pathname .= '/' if ($pathname && (substr($pathname,-1) ne '/'));
-	# # return $pathname;
-# # }
--- conv/FalconConvert.py
+++ conv/FalconConvert.py
@@ -1,53 +1,52 @@
 from datetime import datetime
 from pprint import pprint
-import subprocess, re
+import subprocess, re, os, sys
+from shutil import copyfile
 # import collections
 # import sys, os, logging, multiprocessing
 # from pathlib import Path
 # from pprint import pprint
 
 def run( file, step, ix, param, work):
-    epoch = datetime.now().timestamp()
-    # pprint(step)      
-    file = '.\\' + file    
+    pprint(step)      
+    file = '.\\' + file    ## schon expandiert wg. rglob pathlist
     input_list = [ p for p in step['with-param'] if p['@name'] == 'input' ]
     if len(input_list) != 0:
         input = input_list[0].get( '@value', file ) 
     else:
         input = file
-    print(input)
-    
+    print( os.path.basename( file ) )
+    if file == os.path.basename( file ):
+        file =  work + '\\' + file
+        print(file)
+    input_file_in_work_dir = input.replace('.\data', '.\work')    ## TODO variables
+    copyfile(input, input_file_in_work_dir)   
     format_list = [ p for p in step['with-param'] if p['@name'] == 'format' ]
     if len(format_list) != 0:
         format = format_list[0].get( '@value', 0 ) 
     else:
         print( 'Missing mandatory argument #format#' )
-        sys.exit()
-        
+        sys.exit()        
     script_list = [ p for p in step['with-param'] if p['@name'] == 'script' ]
     if len(script_list) != 0:
         script = script_list[0].get( '@value', 0 ) 
     else:
         print( 'Missing mandatory argument #script#' )
         sys.exit()
-    
     output_list = [ p for p in step['with-param'] if p['@name'] == 'output' ]
     if len(output_list) != 0:
         output = output_list[0].get( '@value', 0 )
     else:
         print( 'Missing mandatory argument #output#' )
         sys.exit()        
-
-    # print( file )   ### schon expandiert
-    # print( script )
-    # print( work )
-    
+    ## path expansions 
     switch = {
       'p' : param, 
       'w' : work,
       'f' : file, 
-      't' : epoch
-    }    
+      't' : datetime.now().timestamp()
+    } 
+    ## TODO: Was ist denn, wenn mehrere matchen und ersetzt werden müssen?    
     query = '\{\$([wpft])\}[\/\\\\](.+)$'
     pattern = re.compile(r"{}".format(query), re.IGNORECASE)   
     m = pattern.search( script )
@@ -58,41 +57,23 @@ def run( file, step, ix, param, work):
     if m:
         expansion = switch[ m.group(1).strip() ] + '\\' + m.group(2).strip() 
         param = expansion
-    m = pattern.search( input )
+    m = pattern.search( input )    
     if m:
         expansion = switch[ m.group(1).strip() ] + '\\' + m.group(2).strip() 
-        input = expansion  
+        input = expansion           
+    ## run sys process
     
-    
-    # for v in [ script, output, format ]:
-        # m = pattern.search( v )
-        # if m:
-            # expansion = switch[ m.group(1).strip() ] + '\\\\' + m.group(2).strip() 
-            # v = expansion
-            # print(expansion)    
-    # print( script )
-    # print( param )    
-  
-    command = f"\"C:\PortablePrograms\Falcon\Falcon.exe\" -d {file} -f {format} -e {script}\{output} -x1 -h1"
-    print( command )   
-    completed_process = subprocess.run( command )    
-    print(completed_process.returncode)
-        # return 
-        
-    # if len(input) != 0:
-        # pprint(  input[0].get( '@value', 0 ) )
-    # if len(format) != 0:
-        # pprint(  format[0].get( '@value', 0 ) )
-    # if len(script) != 0:
-        # pprint(  script[0].get( '@value', 0 ) )
-    # if len(output) != 0:
-
-
-
-    ## for s in [ 'input', 'script', 'output', 'format' ]:
-
-      
-    1   
-    # else:
-        # return 0
+    input_file_in_work_dir = os.path.abspath( input_file_in_work_dir )
+    script = os.path.abspath( param + '\\' + script )
+    output = os.path.abspath( work + '\\' + output )
+    script = os.path.abspath( script )
+    # while not os.path.isfile(input_file_in_work_dir):    
+    command = f"\"C:\PortablePrograms\Falcon\Falcon.exe\" -d {input_file_in_work_dir} -f {format} -e {script}/{output} -x1 -h1"
+    try:
+      completed_process = subprocess.run( command )   
+      print(completed_process.returncode)      
+      return completed_process.returncode    
+    except Error:
+      return 1  
+    return 1
     
