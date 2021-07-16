9b8e39af008f7982de2daa8cef2a541e10a04214
= <=>
2021-07-16 11:53:20 +0200

    Exchange duplicate $f for $p in module System

--- conv/SysMerge.py
+++ conv/SysMerge.py
@@ -7,13 +7,27 @@ import subprocess, os
 # from pprint import pprint
 
 def run( file, step, ix, param, work, data):     
-    epoch = datetime.now().timestamp()
-
-
-
+    expansion = {
+      'p' : param, 
+      'w' : work,
+      'f' : os.path.basename(file), 
+      't' : str(datetime.now().timestamp())
+    } 
+    prms = {}       
+    for p in ['command']:      
+        if isinstance( step['with-param'], list):
+            prms[p] = [ e for e in  step['with-param']  if e['@name'] == p ]
+            prms[p] = next(iter(prms[p]), {})                           ## https://stackoverflow.com/a/23003811/4237436            
+        elif isinstance( step['with-param'], dict):
+            prms[p] = step['with-param'] if step['with-param']['@name'] == p else {}        
+        prms[p] = prms[p].get( '@value', '' )  
+        prms[p] = prms[p].replace( '{$p}', expansion['p'] )
+        prms[p] = prms[p].replace( '{$w}', expansion['w'] )    
+        prms[p] = prms[p].replace( '{$t}', expansion['t'] )
+        prms[p] = prms[p].replace( '{$f}', expansion['f'] )            
+              
     try:
-
-    except: 
-        return 0
-    return 1    
-    
+        completed_process = subprocess.run( prms['command'], shell=True)
+    except:  
+        return 0           
+    return 1           
