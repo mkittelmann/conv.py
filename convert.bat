86e33c443fa844c6d7afe5fbf20c5fd40e5d9cf5
= <=>
2021-07-27 14:56:30 +0200

    Add example to convert.bat

--- convert.bat
+++ convert.bat
@@ -9,5 +9,8 @@ REM python conv.py -q .\data -p .\param -w .\work -i test.xml
 
 REM python conv.py -q .\data -p .\param -w .\work -i test_nur_ein_Modul.xml
 
-python conv.py -q .\data -p .\param -w .\work -i test_nur_ein_Modul.xml -b 1 -e 1
+REM python conv.py -q .\data -p .\param -w .\work -i test_nur_ein_Modul.xml -b 1 -e 1
+REM teste Fehlertoleranz
+REM python conv.py -q .\data -p .\param -w .\work -i test_nur_ein_Modul.xml -b 1 -e 5   
+REM python conv.py -q .\data -p .\param -w .\work -i test.xml -b 2 -e 2
 
