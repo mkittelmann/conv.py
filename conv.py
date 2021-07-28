b5916369c8a3307c693c5cce2c0d4132d0b6a80a
= <=>
2021-07-28 08:08:18 +0200

    Made logzero logger globally available

--- conv.py
+++ conv.py
@@ -18,7 +18,7 @@ from functools import partial
 from pathlib import Path
 from pprint import pprint
 from datetime import datetime
-from logzero import logger  ##logzero.json() ## for log as json
+from logzero import logger  
 ## application specific modules
 import conv.Config
 import conv.FalconConvert 
@@ -29,6 +29,34 @@ import conv.BaseX
 import conv.BaseX_Import
 import conv.System
 # import conv.SysMerge
+
+## early initialization so that -v is available for log settings
+parser=argparse.ArgumentParser()
+parser.add_argument('-q', '-queue', help='', dest='inputdir')    ## https://stackoverflow.com/questions/28638813/how-to-make-a-short-and-long-version-of-a-required-argument-using-python-argpars            
+parser.add_argument('-w', '-work', help='', dest='workdir', required=True)   
+parser.add_argument('-p', '-param', help='', dest='paramdir')   
+parser.add_argument('-i', '-instructions', help='', dest='xmlfile', required=True )   
+parser.add_argument('-b', '-begin', help='Step from which to start (not 0-based)', dest='begin')
+parser.add_argument('-e', '-end', help='Number of steps to perform', dest='end')
+parser.add_argument('-d', '-die', help='', dest='die', action='store_true')
+parser.add_argument('-v', '-verbose', help='', dest='verbose', action='store_true')
+parser.add_argument('-l', '-library', help='', default='$Conv::Config::MODULE_LIBRARY', dest='lib')  ## TODO
+args=parser.parse_args() 
+
+## requires initializing prior to function definition
+logzero.loglevel(level=logzero.INFO)  
+lf = Path(".\conv.log")
+lf.touch(exist_ok=True) 
+try:
+    logzero.logfile(lf, maxBytes=1000000, backupCount=3, loglevel=logging.INFO)                        
+except:
+    logger.error( f"Unable to source log file\n{sys.exc_info()[0]}" )        
+if args.verbose:   
+    logzero.logfile(lf, maxBytes=1000000, backupCount=3, loglevel=logging.DEBUG)                            
+    logzero.setup_logger(fileLoglevel=logzero.DEBUG)
+    logzero.loglevel(logzero.DEBUG)
+    logger.debug("DEBUG logging switched on.")
+logger.debug("DEBUG logging outside of -v setting.")
 
 ########################################################################
 ###                          functions                               ###
@@ -55,21 +83,7 @@ def run_module( file, step, ix, param, work, data):
 ########################################################################
 if __name__ == "__main__":
     start = datetime.now().replace(microsecond=0)       
-    parser=argparse.ArgumentParser()
-    parser.add_argument('-q', '-queue', help='', dest='inputdir')    ## https://stackoverflow.com/questions/28638813/how-to-make-a-short-and-long-version-of-a-required-argument-using-python-argpars            
-    parser.add_argument('-w', '-work', help='', dest='workdir', required=True)   
-    parser.add_argument('-p', '-param', help='', dest='paramdir')   
-    parser.add_argument('-i', '-instructions', help='', dest='xmlfile', required=True )   
-    parser.add_argument('-b', '-begin', help='Step from which to start (not 0-based)', dest='begin')
-    parser.add_argument('-e', '-end', help='Number of steps to perform', dest='end')
-    parser.add_argument('-d', '-die', help='', dest='die')
-    parser.add_argument('-v', '-verbose', help='', dest='verbose')
-    parser.add_argument('-l', '-library', help='', default='$Conv::Config::MODULE_LIBRARY', dest='lib')  ## TODO
-    args=parser.parse_args()  
-    try:
-        logzero.logfile(".\conv.log")                        
-    except:
-        print( "Unable to source log file\n", sys.exc_info()[0] )
+
     logger.info('Converter process started.')
     ## dirs      
     work_dir_in_str=args.workdir
@@ -99,9 +113,9 @@ if __name__ == "__main__":
             wf = [ wf ]        
     except:
         logger.error( f"Unexpected error accessing XML instruction modules. Invalid XML.\n{sys.exc_info()[0]}" )    
-        sys.exit()    
-    
-    ## begin / end  ## nicht 0-basiert
+        sys.exit()        
+
+    ## begin / end  ## begin nicht 0-basiert
     b = int(args.begin) -1 if args.begin else 0
     e = b + int(args.end) if args.end else len(wf) - b
     ## apply begin / end 
@@ -109,7 +123,7 @@ if __name__ == "__main__":
         
     ## execute workflow on each path in pathlist    
     pathlist = Path(data_dir_in_str).rglob('*.*')                
-    run_workflow_partial = partial(run_workflow, workflow=wf, param=os.path.abspath(param_dir_in_str), work=os.path.abspath(work_dir_in_str), data=os.path.abspath(data_dir_in_str))       
+    run_workflow_partial = partial(run_workflow, workflow=wf, param=os.path.abspath(param_dir_in_str), work=os.path.abspath(work_dir_in_str), data=os.path.abspath(data_dir_in_str) )       
     with multiprocessing.Pool() as pool:
         results = pool.map(run_workflow_partial, pathlist) 
         results = [x for x in results if x is not None ]
--- convert.bat
+++ convert.bat
@@ -7,7 +7,7 @@ REM python conv.py -q .\data_xml -p .\param -w .\work -i testbasex.xml
 
 REM python conv.py -q .\data -p .\param -w .\work -i test.xml
 
-REM python conv.py -q .\data -p .\param -w .\work -i test_nur_ein_Modul.xml
+python conv.py -q .\data -p .\param -w .\work -i test_nur_ein_Modul.xml
 
 REM python conv.py -q .\data -p .\param -w .\work -i test_nur_ein_Modul.xml -b 1 -e 1
 REM teste Fehlertoleranz
